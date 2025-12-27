105704dcf77c59480f3c9f4a5d716bafb7272ec2
amirAlimosolo <amirali.mosolo0@gmail.com>
2025-12-27 19:29:44 +0330

    Creating a product category hover and displaying categorized products in each section

--- src/components/module/header/Nav.jsx
+++ src/components/module/header/Nav.jsx
@@ -7,10 +7,116 @@ export default function NavHeader() {
     <div className="second-container flex items-center justify-between py-4 border-b border-Neutral-100/50 max-md:hidden">
       <div className="flex items-center gap-x-14">
         {/* category */}
-        <div className="text-Neutral-800 border border-Neutral-100 rounded-xl cursor-pointer p-2 flex items-center gap-x-2">
-          <IoMenuOutline />
-          <span>دسته بندی محصول</span>
-          <IoIosArrowUp className="-rotate-90" />
+        <div className="relative group/menu">
+          <div className="text-Neutral-800 border border-Neutral-100 rounded-xl cursor-pointer p-2 flex items-center gap-x-2 ">
+            <IoMenuOutline />
+            <span>دسته بندی محصول</span>
+            <IoIosArrowUp className="-rotate-90" />
+
+            <div className="w-[185px] h-0 opacity-0 transition-all duration-500 right-0 top-15 bg-white border border-Neutral-100 z-40 p-2 rounded-lg delay-75 flex gap-y-4 absolute group-hover/menu:h-[400px] group-hover/menu:opacity-100 group-hover/menu:top-12">
+              <ul className="flex flex-col w-full">
+                {/* آیتم ۱ با زیرمنو */}
+                <li className="group/item">
+                  <div className="block w-full py-2 rounded-l-full transition-all duration-500 hover:bg-[#EBF5FE] cursor-pointer group-hover/item:bg-[#EBF5FE]">
+                    متن تستی اول
+                  </div>
+
+                  {/* sub-menu فقط با hover همین آیتم */}
+                  <div
+                    className="w-0 max-h-[400px] overflow-y-auto top-0 absolute rounded-lg bg-white border border-Neutral-100 transition-all duration-500
+             opacity-0 invisible right-[90%]
+            group-hover/item:opacity-100 group-hover/item:visible group-hover/item:right-[104%] group-hover/item:h-auto group-hover/item:w-[800px]"
+                  >
+                    <ul className="flex flex-wrap items-start gap-8 m-3">
+                      <li className="border h-auto border-Neutral-50 rounded-md flex-center  flex-col ">
+                        <a className="!w-[160px] !h-[40px] px-16 py-3 cursor-pointer bg-[#EBF5FE] rounded-t-md">
+                          پرینتر
+                        </a>
+
+                        <ul className="**:w-[160px] **:h-[40px] flex-center **:leading-10 flex-col **:border-t **:text-center **:border-Neutral-50 **:cursor-pointer">
+                          <li>پرینتر سه بعدی</li>
+                          <li>پرینتر حرارتی</li>
+                          <li>پرینتر رنگی</li>
+                          <li>پرینتر حرارتی</li>
+                          <li>پرینتر رنگی</li>
+                        </ul>
+                      </li>
+
+                      <li className="border h-auto border-Neutral-50 rounded-md flex-center  flex-col ">
+                        <a className="!w-[160px] !h-[40px] px-16 py-3 cursor-pointer bg-[#EBF5FE] rounded-t-md">
+                          پرینتر
+                        </a>
+
+                        <ul className="**:w-[160px] **:h-[40px] flex-center **:leading-10 flex-col **:border-t **:text-center **:border-Neutral-50 **:cursor-pointer">
+                          <li>پرینتر سه بعدی</li>
+                        </ul>
+                      </li>
+
+                      <li className="border h-auto border-Neutral-50 rounded-md flex-center  flex-col ">
+                        <a className="!w-[160px] !h-[40px] px-16 py-3 cursor-pointer bg-[#EBF5FE] rounded-t-md">
+                          پرینتر
+                        </a>
+
+                        <ul className="**:w-[160px] **:h-[40px] flex-center **:leading-10 flex-col **:border-t **:text-center **:border-Neutral-50 **:cursor-pointer">
+                          <li>پرینتر سه بعدی</li>
+                          <li>پرینتر حرارتی</li>
+                          <li>پرینتر رنگی</li>
+                          <li>پرینتر سه بعدی</li>
+                          <li>پرینتر حرارتی</li>
+                          <li>پرینتر رنگی</li>
+                          <li>پرینتر سه بعدی</li>
+                          <li>پرینتر حرارتی</li>
+                          <li>پرینتر رنگی</li>
+                        </ul>
+                      </li>
+
+                      <li className="border h-auto border-Neutral-50 rounded-md flex-center  flex-col ">
+                        <a className="!w-[160px] !h-[40px] px-16 py-3 cursor-pointer bg-[#EBF5FE] rounded-t-md">
+                          پرینتر
+                        </a>
+
+                        <ul className="**:w-[160px] **:h-[40px] flex-center **:leading-10 flex-col **:border-t **:text-center **:border-Neutral-50 **:cursor-pointer">
+                          <li>پرینتر سه بعدی</li>
+                          <li>پرینتر حرارتی</li>
+                          <li>پرینتر رنگی</li>
+                        </ul>
+                      </li>
+
+                      <li className="border h-auto border-Neutral-50 rounded-md flex-center  flex-col ">
+                        <a className="!w-[160px] !h-[40px] px-16 py-3 cursor-pointer bg-[#EBF5FE] rounded-t-md">
+                          پرینتر
+                        </a>
+
+                        <ul className="**:w-[160px] **:h-[40px] flex-center **:leading-10 flex-col **:border-t **:text-center **:border-Neutral-50 **:cursor-pointer">
+                          <li>پرینتر سه بعدی</li>
+                          <li>پرینتر حرارتی</li>
+                          <li>پرینتر رنگی</li>
+                        </ul>
+                      </li>
+
+                      <li className="border h-auto border-Neutral-50 rounded-md flex-center  flex-col ">
+                        <a className="!w-[160px] !h-[40px] px-16 py-3 cursor-pointer bg-[#EBF5FE] rounded-t-md">
+                          پرینتر
+                        </a>
+
+                        <ul className="**:w-[160px] **:h-[40px] flex-center **:leading-10 flex-col **:border-t **:text-center **:border-Neutral-50 **:cursor-pointer">
+                          <li>پرینتر سه بعدی</li>
+                          <li>پرینتر حرارتی</li>
+                          <li>پرینتر رنگی</li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li className="block w-full py-2 rounded-l-full transition-all duration-500 hover:bg-[#EBF5FE] cursor-pointer">
+                  متن تستی دوم
+                </li>
+                <li className="block w-full py-2 rounded-l-full transition-all duration-500 hover:bg-[#EBF5FE] cursor-pointer">
+                  متن تستی سوم
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
         {/* nav */}
         <div>
